8a6e6fb25d08993076389a6316cdc17ac17f45b4
Ahmedshaikh1899 <ahmedshaikh1899@gmail.com>
2026-08-22 12:06:01 +0530

    Add security group for database server with TLS
    added db srv secgrp

--- security-grp.tf
+++ security-grp.tf
@@ -17,3 +17,12 @@ resource "aws_security_group" "app_vpc_secgrp" {
     Name = "allow_tls"
   }
 }
+resource "aws_security_group" "db_vpc_secgrp" {
+  name        = "allow_dbsrv"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.lab_vpc.id
+
+  tags = {
+    Name = "allow_dbsrv_tls"
+  }
+}
