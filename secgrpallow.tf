ae105ab375c046a0a3420505acc565eed63e990e
Ahmedshaikh1899 <ahmedshaikh1899@gmail.com>
2026-08-22 12:13:34 +0530

    Add security group rules for database access

--- secgrpallow.tf
+++ secgrpallow.tf
@@ -38,3 +38,18 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_app_traffic_ipv6" {
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+###############  DB SERVERS - SECURITY GROUP###########
+resource "aws_vpc_security_group_ingress_rule" "allow_db_ipv4" {
+  security_group_id = aws_security_group.db_vpc_secgrp.id
+  cidr_ipv4         = "10.0.0.0/16"
+  from_port         = 8080
+  ip_protocol       = "tcp"
+  to_port           = 8080
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_db_traffic_ipv4" {
+  security_group_id = aws_security_group.app_vpc_secgrp.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
